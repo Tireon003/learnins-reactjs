595fda7560afb09ea9e7a12545181f0ed40310b7
Tireon003 <gunash29@mail.ru>
2023-04-19 14:43:24 +0300

    some decomposition, created new dir pages and added new page

--- src/App.js
+++ src/App.js
@@ -11,52 +11,14 @@ import CreatePostForm from './components/UI/CreatePostForm/CreatePostForm.jsx';
 import Loader from './components/UI/Loader/Loader.jsx';
 import { useFetching } from './hooks/useFetching.jsx';
 import Header from './components/Header/Header.jsx';
+import PostPage from './components/pages/PostPage.jsx';
 
 function App() {
 
-  const [posts, setPosts] = useState([]);
-
-  //to keep query from search in context InputQuery
-  const [query, setQuery] = useState('');
-
-  const [showModal, setShowModal] = useState(false);
-
-  const [newPostData, setNewPostData] = useState({key: Date.now(), title: '', body: ''});
-  const [loadPosts, isPostsLoading, postError] = useFetching(async () => {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
-    setPosts(response.data);
-  })
   
-  useEffect(() => {
-    loadPosts()
-  }, []);
-
-const createPost = () => {
-    if(newPostData.title && newPostData.body) {
-      setPosts([newPostData, ...posts]);
-      setShowModal(false); 
-    }
-  }
 
   return (
-    <InputQuery.Provider value={[query, setQuery]}>
-      <NewPostData.Provider value={[newPostData, setNewPostData]}>
-          <div className="App">
-            <Header/>
-            <div className='page_body'>
-              <Modal visible={showModal} setVisible={setShowModal}>
-                <CreatePostForm createPost={createPost}/>
-              </Modal>
-              <Panel posts={posts} setVisible={setShowModal}/>
-              {postError && <h1 style={{textAlign: "center", marginTop: 20}}>Произошла ошибка... {postError}</h1>}
-              {isPostsLoading
-                ? <div style={{display: "flex", justifyContent: "center", marginTop: 60}}><Loader/></div>
-                : <PostList posts={posts}/>}
-            </div>
-          </div>
-            
-      </NewPostData.Provider>
-    </InputQuery.Provider>
+    <PostPage/>
   );
 }
 
---
+++ src/components/pages/PostPage.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import { InputQuery, NewPostData } from '../../context.js';
+import axios from 'axios';
+
+import PostList from '../UI/PostList/PostList';
+import Panel from '../UI/Panel/Panel';
+import Modal from '../UI/Modal/Modal.jsx';
+import CreatePostForm from '../UI/CreatePostForm/CreatePostForm';
+import Loader from '../UI/Loader/Loader.jsx';
+import { useFetching } from '../../hooks/useFetching.jsx';
+import Header from '../Header/Header.jsx';
+
+const PostPage = () => {
+    
+    const [posts, setPosts] = useState([]);
+
+  //to keep query from search in context InputQuery
+  const [query, setQuery] = useState('');
+
+  const [showModal, setShowModal] = useState(false);
+
+  const [newPostData, setNewPostData] = useState({key: Date.now(), title: '', body: ''});
+  const [loadPosts, isPostsLoading, postError] = useFetching(async () => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+    setPosts(response.data);
+  })
+  
+  useEffect(() => {
+    loadPosts()
+  }, []);
+
+const createPost = () => {
+    if(newPostData.title && newPostData.body) {
+      setPosts([newPostData, ...posts]);
+      setShowModal(false); 
+    }
+  }
+    
+    return (
+        <InputQuery.Provider value={[query, setQuery]}>
+      <NewPostData.Provider value={[newPostData, setNewPostData]}>
+          <div className="App">
+            <Header/>
+            <div className='page_body'>
+              <Modal visible={showModal} setVisible={setShowModal}>
+                <CreatePostForm createPost={createPost}/>
+              </Modal>
+              <Panel posts={posts} setVisible={setShowModal}/>
+              {postError && <h1 style={{textAlign: "center", marginTop: 20}}>Произошла ошибка... {postError}</h1>}
+              {isPostsLoading
+                ? <div style={{display: "flex", justifyContent: "center", marginTop: 60}}><Loader/></div>
+                : <PostList posts={posts}/>}
+            </div>
+          </div>
+            
+      </NewPostData.Provider>
+    </InputQuery.Provider>
+    );
+}
+
+export default PostPage;
